fa9c59d142812d299a1933c530aab6dadc0675f9
Marree33 <48028741+marree33@users.noreply.github.com>
2021-04-07 19:20:44 -0500

    Update urls.py
    Updated views.py for the 2 Get Involved Forms and the 2 Darkness to Light Forms. These forms will submit forms to a clients email after the necessary credentials(email and pass) are given and put in settings.py

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,9 @@ from .import views
 
 urlpatterns = [
 			path('',views.home_view),
+			path('attendtraining/' , views.attendTraining),
+    			path('scheduletraining/' , views.scheduleTraining),
+    			path('host-event/' , views.hostEvent),
+    			path('volunteer-event/' , views.volunteerEvent),
+			
 			]
